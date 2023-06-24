7bb66ce117df0d9b9c0655c644792c6ffe907b39
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-24 20:43:06 +0530

    base url added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- baseURL: process.env.REACT_APP_BASE_URL,
+ baseURL: 'https://chat-zone-server-api.onrender.com',
 })
 
 export default axiosInstance;
